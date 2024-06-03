ca9944f77c2d08c22ef397e76bf667bfc13095c5
Siddhant Gupta <sidgupta1202@gmail.com>
2024-06-04 04:10:12 +0530

    colors

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color mainColor = Color(0xFF00BB61);
-const operatorColor = Color.fromARGB(255, 61, 61, 61);
+const operatorColor = Color.fromARGB(255, 255, 255, 255);
 const buttonColor = Color.fromARGB(255, 0, 0, 0);
 const orangeColor =  Color(0xFF00BB61);
 
